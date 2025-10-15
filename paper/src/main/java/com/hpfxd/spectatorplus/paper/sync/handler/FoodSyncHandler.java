7c157d37b065b616ff9964d384ef529b09c473b8
packetsss <pyjpanyanjunpyj@gmail.com>
2025-10-14 22:45:27 -0500

    Refactor FoodSyncHandler to improve food level synchronization and add periodic updates

--- paper/src/main/java/com/hpfxd/spectatorplus/paper/sync/handler/FoodSyncHandler.java
+++ paper/src/main/java/com/hpfxd/spectatorplus/paper/sync/handler/FoodSyncHandler.java
@@ -12,6 +12,7 @@ import org.bukkit.event.entity.FoodLevelChangeEvent;
 
 public class FoodSyncHandler implements Listener {
     private static final String PERMISSION = "spectatorplus.sync.food";
+    private static final long UPDATE_INTERVAL_TICKS = 60; // roughly every 3 seconds
 
     private final SpectatorPlugin plugin;
 
@@ -19,6 +20,7 @@ public class FoodSyncHandler implements Listener {
         this.plugin = plugin;
 
         Bukkit.getPluginManager().registerEvents(this, plugin);
+        Bukkit.getScheduler().runTaskTimer(plugin, this::broadcastPeriodicUpdates, UPDATE_INTERVAL_TICKS, UPDATE_INTERVAL_TICKS);
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
@@ -31,7 +33,8 @@ public class FoodSyncHandler implements Listener {
             }
 
             final Player dataPlayer = forced != null ? forced : player;
-            this.plugin.getSyncController().broadcastPacketToSpectators(dataPlayer, PERMISSION, new ClientboundFoodSyncPacket(dataPlayer.getUniqueId(), event.getFoodLevel(), dataPlayer.getSaturation()));
+            final int foodLevel = forced != null ? dataPlayer.getFoodLevel() : event.getFoodLevel();
+            this.sendHungerUpdate(dataPlayer, foodLevel, dataPlayer.getSaturation());
         }
     }
 
@@ -43,5 +46,26 @@ public class FoodSyncHandler implements Listener {
             final Player dataPlayer = this.plugin.getSyncController().resolveDataPlayer(target);
             this.plugin.getSyncController().sendPacket(spectator, new ClientboundFoodSyncPacket(dataPlayer.getUniqueId(), dataPlayer.getFoodLevel(), dataPlayer.getSaturation()));
         }
+    }
+
+    private void broadcastPeriodicUpdates() {
+        final Player forced = this.plugin.getSyncController().getForcedSyncPlayer().orElse(null);
+
+        if (forced != null) {
+            this.broadcastCurrentState(forced);
+            return;
+        }
+
+        for (final Player player : Bukkit.getOnlinePlayers()) {
+            this.broadcastCurrentState(player);
+        }
+    }
+
+    private void broadcastCurrentState(Player dataPlayer) {
+        this.sendHungerUpdate(dataPlayer, dataPlayer.getFoodLevel(), dataPlayer.getSaturation());
+    }
+
+    private void sendHungerUpdate(Player dataPlayer, int foodLevel, float saturation) {
+        this.plugin.getSyncController().broadcastPacketToSpectators(dataPlayer, PERMISSION, new ClientboundFoodSyncPacket(dataPlayer.getUniqueId(), foodLevel, saturation));
     }
 }
